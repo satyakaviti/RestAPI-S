7f2ae736ce6fbe13de5f11da73914c810db5c14f
SATYA KAVITI <satya.kaviti@instanpe.com>
2024-12-10 17:53:00 +0530

    commit contains repo details regarding loans

---
+++ src/main/java/com/payments/loans/LoansApplication.java
@@ -0,0 +1,40 @@
+package com.payments.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.payments.loans.controller") })
+@EnableJpaRepositories("com.payments.loans.repository")
+@EntityScan("com.payments.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "SatyamBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Satya_Kaviti",
+						email = "satya@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "SatyamBank Loans microservice REST API Documentation"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
